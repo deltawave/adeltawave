d373c03cd7bd6da852bb3fc3d5921a56c48d853f
sergei <sergei.sapoznikov@gmail.com>
2011-10-02 20:51:35 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adeltawave::Application.config.secret_token = 'f2fd7112099984e93c539f8b1c94870eccb2b5e3715da217dd4efc8c21708f85c63428c489e45fb571fcbb4ae6ec320940b6f38b200800d1900d91513a4cb5d3'
